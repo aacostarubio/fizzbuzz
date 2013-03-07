abfff2fe19a02ecc31296eee5efe264a76e1181e
Alejandro Acosta-Rubio <mastermind@Alejandros-MacBook-Air.local>
2013-03-07 14:49:05 -0600

    fizz is passing

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,7 +10,7 @@ describe FizzBuzz do
 		output_for(1..1).should == '1'
 	end
 
-	it 'should output 1 for this try' do
+	it 'should output fizz for this try' do
 		output_for(3..3).should == 'fizz'
 	end
 
