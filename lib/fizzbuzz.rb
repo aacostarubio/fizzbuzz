efcbffa6c9f34049b20c352033abe455683c4b3c
Alejandro Acosta-Rubio <mastermind@Alejandros-MacBook-Air.local>
2013-03-20 11:54:55 -0500

    added description

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,24 +1,23 @@
 class FizzBuzz
-	attr_reader :range
+ attr_reader :range
 
-	def initialize(range)
-		@range = range
-	end
+ def initialize(range)
+ 	 @range = range
+ end
 
-	def play
-		range.map { |try| output_for_number(try) }
-	end
+ def play
+   range.map { |try| output_for_number(try) }
+ end
 
-	def output_for_number(number)
-		if (number % 15).zero?
-			"fizzbuzz"
-		elsif (number % 3).zero?
-			"fizz"
-		elsif (number % 5).zero?
-			"buzz"
-		else
-			number
-		end
-	end
-
+ def output_for_number(number)
+  if (number % 15).zero?
+    "fizzbuzz"
+  elsif (number % 3).zero?
+    "fizz"
+  elsif (number % 5).zero?
+    "buzz"
+  else
+    number
+  end
+ end
 end
